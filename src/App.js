a50c0749269292f58e08f70e89173ec7cadc9fd0
poseidon-code <pritamhalder05062000@gmail.com>
2020-12-09 17:33:18 +0530

    added Narration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Player from './components/Player'
-import Settings from './components/Settings'
 import Story from './components/Story'
 
 const App = () => {
     const [darkmode, setDarkmode] = useState(false)
-    const [openSettings, setOpenSettings] = useState(false)
-
     const [story, setStory] = useState({
         title: '',
         author: '',
@@ -43,20 +40,14 @@ const App = () => {
         setDarkmode(!darkmode)
     }
 
-    const toggleOpenSettings = () => {
-        setOpenSettings(!openSettings)
-    }
-
     return (
         <div className={`w-full min-h-screen ${darkmode ? 'dark' : ''}`}>
             <div className='relative min-h-screen w-full bg-gray-100 dark:bg-gray-900'>
-                <Navbar toggleDarkmode={toggleDarkmode} isDark={darkmode} toggleOpenSettings={toggleOpenSettings} />
+                <Navbar toggleDarkmode={toggleDarkmode} isDark={darkmode} />
 
                 <Story story={story} />
 
-                {openSettings && <Settings toggleOpenSettings={toggleOpenSettings} />}
-
-                <Player />
+                <Player story={story} />
             </div>
         </div>
     )
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,39 @@
-const Player = () => {
+import { Pause, Play, Stop } from './Icons'
+import { useSpeechSynthesis } from 'react-speech-kit'
+
+const Player = ({ story }) => {
+    const { speak, speaking, cancel } = useSpeechSynthesis()
+
+    const play = () => {
+        speak({
+            text: `
+            ${story.title}.
+            '\nby : '${story.author}.
+            '\n\n'${story.story}.
+            '\n\nMoral : '${story.moral}`,
+        })
+    }
+
+    const stop = () => {
+        cancel()
+    }
+
     return (
         <footer className='fixed w-full z-10 bottom-0 bg-purple-600 dark:bg-purple-800 py-3'>
-            <div className='md:w-3/4 md:mx-auto w-full'>
-                <h2 className='text-2xl text-white'>Player</h2>
+            <div className='md:w-3/4 md:mx-auto w-full flex items-center justify-center space-x-4'>
+                <button
+                    className='p-2 rounded-full hover:bg-purple-500 dark:hover:bg-purple-600 transition duration-300 focus:outline-none'
+                    onClick={speaking ? stop : play}>
+                    {speaking ? <Pause /> : <Play />}
+                </button>
+                <button
+                    className={`${
+                        speaking ? '' : 'disabled:opacity-50 disabled:pointer-events-none'
+                    } p-2 rounded-full hover:bg-purple-500 dark:hover:bg-purple-600 transition duration-300 focus:outline-none  disabled:hover:bg-transparent`}
+                    disabled={speaking ? false : true}
+                    onClick={stop}>
+                    <Stop />
+                </button>
             </div>
         </footer>
     )
