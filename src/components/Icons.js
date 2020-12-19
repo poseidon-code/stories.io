6d4f26db3c9fb7f2a71b63a6b1fd513c46dd3680
poseidon-code <pritamhalder05062000@gmail.com>
2020-12-19 17:06:48 +0530

    added Code icon

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -20,6 +20,12 @@ export const Dark = () => (
     </svg>
 )
 
+export const Code = () => (
+    <svg className='w-8 h-8 text-white' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4' />
+    </svg>
+)
+
 export const Github = () => (
     <svg
         className='w-8 h-8 p-1'
